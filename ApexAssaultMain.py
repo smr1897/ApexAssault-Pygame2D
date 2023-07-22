6e8af689ef5a52705c27d74d0b6ab2a074443c37
sahan <sahan.m.rajapaksha@gmail.com>
2023-07-22 23:52:53 +0530

    second

--- ApexAssaultMain.py
+++ ApexAssaultMain.py
@@ -9,9 +9,17 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Apex Assault')
 
+class Soldier(pygame.sprite.Sprite):
+    def __init__(self,x,y,scale):
+        pygame.sprite.Sprite.__init__(self)
+        
+
 x = 200
 y = 200
 img = pygame.image.load('images/player/idle/idle.png')
+#Scale the image
+#scale = 2
+#img = pygame.transform.scale(img,(img.get_width()*scale,img.get_height()*scale))
 rect = img.get_rect()
 rect.center = (x,y)
 
